bd91314370fa26e611bda1954c900c0f7f7ab459
CapitaineToinon <antoinesutter@gmail.com>
2023-04-27 23:18:47 +0200

    Progress

---
+++ src/lib/components/slides/06_musts.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Excalidraw from '$lib/components/Excalidraw.svelte';
+</script>
+
+<Excalidraw url="https://link.excalidraw.com/readonly/3YrF8fLbSvw58favLqU5" />
--- src/lib/components/slides/index.js
+++ src/lib/components/slides/index.js
@@ -3,6 +3,8 @@ import Slide02 from './02.svelte';
 import Slide03 from './03.svelte';
 import Slide04 from './04.svelte';
 import Slide05 from './05_disclaimer.svelte';
+import Slide06 from './06_musts.svelte';
+import Slide07 from './07_nices.svelte';
 import Slide99 from './99.svelte';
 
 export const slides = [
@@ -11,5 +13,7 @@ export const slides = [
 	{ name: 'Context', chapter: 'Introduction', component: Slide03 },
 	{ name: 'The problem', chapter: 'Introduction', component: Slide04 },
 	{ name: 'Disclaimer', chapter: 'Introduction', component: Slide05 },
+	{ name: 'Must haves', chapter: 'Requirements', component: Slide06 },
+	{ name: 'Nice to haves', chapter: 'Requirements', component: Slide07 },
 	{ name: 'Thank You', chapter: 'The end', component: Slide99 },
 ];
